11ebad175908b1fdf5069cf26651c752d80a5f38
sarahoyne <smh418@lehigh.edu>
2015-08-31 21:55:01 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,17 @@
+//define a class
+public class WelcomeClass{
+    
+//add main method
+public static void main(String[ ] args) {
+    
+System.out.println("    -----------");
+ System.out.println("    | WELCOME | ");
+System.out.println("  ^  ^  ^  ^  ^  ^");
+System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+System.out.println("<-S==M==H==4==1==8->");
+System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+ System.out.println("  v  v  v  v  v  v");
+ System.out.println("I really enjoy hiking and skiing and just being outdoors in general.");
+ 
+}
+}
