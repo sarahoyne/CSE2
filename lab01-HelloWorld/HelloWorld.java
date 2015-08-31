7414f92c149224e927167fd87464ef08a9124482
sarahoyne <smh418@lehigh.edu>
2015-08-31 20:41:45 +0000

    upddated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,26 @@
+//Sarah Hoyne
+//Hello World Java Program
+//
+//  first compile the program
+//      javac Helloworld.java
+//  run the program
+//      java HelloWorld//
+
+//define a class
+public class HelloWorld{
+    
+//add main method
+public static void main(String[ ] args) {
+
+// print the statement with the box around it
+    System.out.println("###########################");
+    System.out.println("###########################");
+    System.out.println("#                         #");
+    System.out.println("#    Hello World 2014!!   #");
+    System.out.println("#                         #");
+    System.out.println("###########################");
+    System.out.println("test");
+    
+    //test/testtest
+}
+}
