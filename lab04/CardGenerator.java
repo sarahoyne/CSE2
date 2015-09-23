fb9869c026b7f3a55369fa094b33a46313144976
sarahoyne <smh418@lehigh.edu>
2015-09-23 02:39:55 +0000

    updated hw04

---
+++ lab04/CardGenerator.java
@@ -0,0 +1,12 @@
+public class CardGenerator {
+    //main method required for every Java program
+    public static void main(String[] args) {
+      int randomVariable = (int)(Math.random()*13)+1;
+      //System.out.print(randomVariable);
+      int randomSuit= (int)(Math.random()*4)+1;
+      System.out.println(randomSuit);
+       
+       
+        }
+       
+    }
