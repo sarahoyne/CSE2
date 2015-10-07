bca806c2775c37719258476bf7206b0312d96d63
sarahoyne <smh418@lehigh.edu>
2015-10-07 02:56:35 +0000

    updated BarGraph

--- hw06/BarGraph.java
+++ hw06/BarGraph.java
@@ -18,6 +18,92 @@ public class BarGraph {
     double saturdayExpenses = myScanner2.nextDouble();
     System.out.print("Enter Sunday's expenses in the form xxx.xx");
     double sundayExpenses = myScanner2.nextDouble();
+    
+int monday=0;
+int tuesday=0;
+int wednesday=0;
+int thursday=0;
+int friday=0;
+int saturday=0;
+int sunday=0;
+
+if(( mondayExpenses*10- (int)(tuesdayExpenses*10))) >= 5) {                                                                                                                                                              mondayExpenses*10-(int)(mondayExpenses*10)>=5) {
+    monday= (int)(mondayExpenses) + 1;
+}
+else monday=mondayExpense;
+
+if( tuesdayExpenses*10-(int)(tuesdayExpenses*10) >=5) {
+    tuesday=(int)(tuesdayExpenses) + 1;
+}
+else tuesday=tuesdayExpenses;
+
+if( wednesdayExpenses*10-(int)(wednesdayExpenses*10) >=5 ) {
+    wednesday=(int)(wednesdayExpenses) + 1;
+}
+else wednesday=wednesdayExpenses;
+
+if( thursdayExpenses*10-(int)(thursdayExpenses*10)>=5) {
+    thursday=(int)(thursdayExpenses)+1;
+}
+else thursday=thursdayExpenses;
+
+if( fridayExpenses*10-(int)(fridayExpenses*10)>=5) {
+    friday=(int)(fridayExpenses)+1;
+}
+else friday=fridayExpenses;
+
+if( saturdayExpenses*10-(int)(saturdayExpenses*10)>=5) {
+    saturday=(int)(saturdayExpenses)+1;
+}
+else saturday=saturdayExpenses;
+
+if( sundayExpenses*10-(int)(sundayExpenses*10)>=5) {
+    sunday=(int)(sundayExpenses)+1;
+}
+else sunday=sundayExpenses;
+
+System.out.print("Mon: ");
+for(int i = 0; i < monday; i++) {
+    System.out.print("*");
+}
+
+System.out.println();
+System.out.print("Tues: ");
+for(int i= 0; i < tuesday; i++) {
+    System.out.print("*");
+}
+
+System.out.println();
+System.out.print("Wed: ");
+for(int i = 0; i < wednesday; i++) {
+    System.out.print("*");
+}
+
+System.out.println();
+System.out.print("Thrus: ");
+for(int i = 0; i < thursday; i++) {
+    System.out.print("*");
+}
+
+System.out.println();
+System.out.print("Fri: ");
+for(int i = 0; i < wednesday; i++) {
+    System.out.print("*");
+}
+
+System.out.println();
+System.out.print("Sat: ");
+for(int i = 0; i < saturday; i++) {
+    System.out.print("*");
+}
+
+System.out.println();
+System.out.print("Sun: ");
+for(int i = 0; i < sunday; i++) {
+    System.out.print("*")
+}
+
 
     }
+
 }
